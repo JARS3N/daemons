4ee0b1856540eb86b117eb983999ef7950ad8bbc
JARS3N <JARS3N@users.noreply.github.com>
2017-12-20 15:35:13 -0500

    Create wake.R

---
+++ R/wake.R
@@ -0,0 +1,7 @@
+wake<-function(){
+  require(cronR)
+  cmd_dryqc<-cron_rscript(system.files(package='daemons',path='cron','dryqc.R'))
+  cmd_mv<-cron_rscript(system.files(package='daemons',path='cron','mv.R'))
+  cron_add(command = cmd_dryqc,frequency = 'daily', at='1:01', id = 'dryqc')
+  cron_add(command = cmd_mv,frequency = 'daily', at='2:01', id = 'mv')
+}
