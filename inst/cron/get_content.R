68903110551fe66a086ed02b95d915bfb72adbb7
JARS3N <JARS3N@users.noreply.github.com>
2018-01-19 12:05:55 -0500

    Update get_content.R

--- inst/cron/get_content.R
+++ inst/cron/get_content.R
@@ -29,7 +29,7 @@ parse_sub2_context<-function(DIR){
   FLS <- file.path(list.dirs(DIR,full.names = T,recursive = F),"context.xml")
    #index_FLS<-grep("^[0-9]",basename(dirname(FLS)))
   exists<-sapply(FLS,file.exists)
-  size.of.list <- length(index_FLS);
+  size.of.list <- sum(exists);
   cl <- makeCluster( min(size.of.list, detectCores()) );
   work<-parallel::parLapply(cl=cl,FLS[exists],get_context_results)
   null_check<-sapply(work,is.null)
