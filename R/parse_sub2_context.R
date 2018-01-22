9772648fe30fc330b739fb4467e351d24155ae15
JARS3N <JARS3N@users.noreply.github.com>
2018-01-22 11:39:33 -0500

    Update parse_sub2_context.R

--- R/parse_sub2_context.R
+++ R/parse_sub2_context.R
@@ -1,23 +1,8 @@
 
 parse_sub2_context<-function(DIR){
-  db_state<-is_it_in_db_already(DIR)
-  cat(
-      "Is the Lot in the Database:",
-      db_state,
-      "\n")
-  if(db_state){
-    cat(
-      "Lot:",
-      basename(DIR),
-      "is already in the Database. Moving on!\n"
-      )
-    return(NULL)
-  }else{
-    cat("Data required.\n")
-  }
+ bars<-"###########\n"
   require(parallel);
   big_start <- Sys.time()
-  # FLS <- file.path(list.dirs(DIR,full.names = T,recursive = F),"context.xml")
   dir_fls <- list.dirs(DIR,full.names = T,recursive = F)
   FLS <- unlist(sapply(dir_fls,list.files,pattern="context.xml",full.names=T))
   if(length(FLS)<1){return(NULL)}else{
@@ -31,16 +16,18 @@ parse_sub2_context<-function(DIR){
   dbWriteTable(con, name="mv_context",value= DATA, append=TRUE,overwrite = FALSE,row.names=FALSE)
   big_end<- Sys.time()
  msg_wrote<-paste0(
+   bars,
     "wrote Lot ",
     basename(DIR),
     " to table at ",
     Sys.time(),
+   "\n",
     " process took ",
     big_end-big_start,
     " seconds."
     )
   message(msg_wrote)
   dbDisconnect(con)
-  message("disconnect")
+  message("disconnected from db")
 }
 
